5a191bf7c52491192060bdea665197fa527bbec0
Filip Godziński <godzinski.filip@gmail.com>
2018-01-05 20:42:57 +0100

    hide menu after click

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ $(document).ready(function() {
 
   var hamburgerBtn = $("#hamburger");
   var menu = $("#main_nav");
+  var manuLink = $("#main_nav a");
+
 
   menu.hide();
 
   hamburgerBtn.click(function () {
     menu.slideToggle('slow');
     $(this).toggleClass('open');
+  });
+
+  manuLink.click(function () {
+    menu.hide();
+    hamburgerBtn.toggleClass('open');
   });
 });
