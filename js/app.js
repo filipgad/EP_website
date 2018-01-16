a11dc0dbd3d541ea800f5712a6fb519fc799eba0
Filip Godziński <godzinski.filip@gmail.com>
2018-01-16 22:38:58 +0100

    added smoother scroll

--- js/app.js
+++ js/app.js
@@ -2,18 +2,26 @@ $(document).ready(function() {
 
   var hamburgerBtn = $("#hamburger");
   var menu = $("#main_nav");
-  var manuLink = $("#main_nav a");
+  var menuLink = $("#main_nav a");
 
   if (window.matchMedia('(max-width: 767px)').matches) {
     menu.hide();
-    hamburgerBtn.click(function () {
+    hamburgerBtn.on("click", function () {
       menu.slideToggle('slow');
       $(this).toggleClass('open');
     });
 
-    manuLink.click(function () {
+    menuLink.on("click", function () {
       menu.hide();
       hamburgerBtn.toggleClass('open');
     });
   }
+
+  menuLink.on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1000);
+	});
+
 });
