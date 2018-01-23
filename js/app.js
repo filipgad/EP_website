ba6ebb24622f215c23047502b55049f9ede96167
Filip Godziński <godzinski.filip@gmail.com>
2018-01-23 23:08:31 +0100

    added addListener

--- js/app.js
+++ js/app.js
@@ -4,6 +4,25 @@ $(document).ready(function() {
   var menu = $("#main_nav");
   var menuLink = $("#main_nav a");
 
+  // var mobile = window.matchMedia("(max-width: 700px)");
+  //
+  // mobile.addListener(function(mobile) {
+  //   if (mobile.matches) {
+  //     menu.hide();
+  //     hamburgerBtn.on("click", function () {
+  //       menu.slideToggle('slow');
+  //       $(this).toggleClass('open');
+  //     });
+  //
+  //     menuLink.on("click", function () {
+  //       menu.hide();
+  //       hamburgerBtn.toggleClass('open');
+  //     });
+  //   }
+  //   else {
+  //     menu.show();
+  //   }
+  // });
   if (window.matchMedia('(max-width: 767px)').matches) {
     menu.hide();
     hamburgerBtn.on("click", function () {
