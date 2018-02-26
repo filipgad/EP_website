84a519f323f7e773458a2595cb4f8f04872a5ba8
Filip Godziński <godzinski.filip@gmail.com>
2018-02-26 19:57:52 +0100

    actions for scroll to top btn

--- js/app.js
+++ js/app.js
@@ -1,46 +1,43 @@
 $(document).ready(function() {
 
-  var hamburgerBtn = $("#hamburger");
-  var menu = $("#main_nav");
-  var menuLink = $("#main_nav a");
+    var hamburgerBtn = $("#hamburger");
+    var menu = $("#main_nav");
+    var menuLink = $("#main_nav a");
+    var btn = $('#scrrol__btn');
+    
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        menu.hide();
+        hamburgerBtn.on("click", function () {
+            menu.slideToggle('slow');
+            $(this).toggleClass('open');
+        });
 
-  // var mobile = window.matchMedia("(max-width: 700px)");
-  //
-  // mobile.addListener(function(mobile) {
-  //   if (mobile.matches) {
-  //     menu.hide();
-  //     hamburgerBtn.on("click", function () {
-  //       menu.slideToggle('slow');
-  //       $(this).toggleClass('open');
-  //     });
-  //
-  //     menuLink.on("click", function () {
-  //       menu.hide();
-  //       hamburgerBtn.toggleClass('open');
-  //     });
-  //   }
-  //   else {
-  //     menu.show();
-  //   }
-  // });
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    menu.hide();
-    hamburgerBtn.on("click", function () {
-      menu.slideToggle('slow');
-      $(this).toggleClass('open');
-    });
+        menuLink.on("click", function () {
+            menu.hide();
+            hamburgerBtn.toggleClass('open');
+        });
+    }
 
-    menuLink.on("click", function () {
-      menu.hide();
-      hamburgerBtn.toggleClass('open');
-    });
-  }
-
-  menuLink.on('click', function(event) {
+    menuLink.on('click', function(event) {
 		event.preventDefault();
 		$('html, body').animate({
 			scrollTop: $($(this).attr('href')).offset().top
 		}, 1000);
-	});
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200){
+            btn.fadeIn();
+        }else{
+            btn.fadeOut();
+        }
+    });
+    
+    btn.on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
 
 });
